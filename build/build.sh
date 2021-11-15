e6fbbac9ee41c75183793989e9ca854f5458357a
David Kern <david@mju.io>
2021-11-14 19:18:07 -0800

    install startship and configure zsh

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/bin/bash -eux
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -11,9 +11,21 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
 
+# install additional apt packages
+xargs -a packages apt-get install -y
+
+# install starship
+tar zxvf starship-1.0.0-x86_64-unknown-linux-musl.tar.gz -C /usr/bin
+chown root:root /usr/bin/starship
+
 # create users
-useradd -m user
-useradd -m tool
+useradd -m user -s /bin/zsh
+cp -R files/home/user/* /home/user
+chown -R user:user /home/user
+
+useradd -m tool -s /bin/zsh
+cp -R files/home/tool/* /home/tool
+chown -R tool:tool /home/tool
 
 # create /work volume owned by user
 mkdir /work
